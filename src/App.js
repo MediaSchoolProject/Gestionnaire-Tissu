7eb376426b4df5aef7af6352a6663127b30af8fa
wael-1234 <ben.abderrahmen.wael@gmail.com>
2022-05-08 10:34:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import image01 from './pictures/image01.jfif';
+import Navbar from './components/navbar/Navbar';
+import Cart from './components/cart/Cart';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Cart/>
+    <div>
+    <h1 className='title' style={{color:'green'}}> Hi World</h1>
+      <p>j’ai vu beaucoup de célèbres tableaux, comme La Joconde de Leonard De Vinci.</p>
+      <img src={image01} alt=''/>
+      <br/>
+      <button className='btn btn-primary'> click me! </button>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-md">
+    <a className="navbar-brand" href="https://google.com">Navbar</a>
+  </div>
+</nav>
+  )
+}
